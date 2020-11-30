b3564ef65a3e2354a38ed59edc02b6661ecde253
Minwook11 <alsdnr4874@gmail.com>
2020-11-30 11:56:59 +0900

    Fix : Allowed Host 옵션 설정

--- herren_test/settings.py
+++ herren_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4ojqzc1^x0jjibd_n5d)tyzg9u)zcgj7_yo&&xu=7%zwo(0h)o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
