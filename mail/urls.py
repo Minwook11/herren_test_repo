c8e957d3569fcf834a039f4aef40a7385ef01bce
Minwook11 <alsdnr4874@gmail.com>
2020-11-30 11:07:20 +0900

    Add : 메일 송신 API 작성 - 개인, 전체 송신 두 경우로 나누어 처리

--- mail/urls.py
+++ mail/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     SubscribeView,
-    UnsubscribeView
+    UnsubscribeView,
+    SendView
 )
 
 urlpatterns = [
     path('/subscribe', SubscribeView.as_view()),
     path('/unsubscribe', UnsubscribeView.as_view()),
+    path('/send', SendView.as_view()),
 ]
--- mail/views.py
+++ mail/views.py
@@ -1,10 +1,26 @@
 import json
 import re
+import requests
 
 from django.views import View
 from django.http import JsonResponse
 
 from .models import Address
+
+def send_mail(address, subject, content):
+    url = 'http://python.recruit.herrencorp.com/api/v1/mail'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization' : 'herren-recruit-python'
+    }
+    data = {
+        'mailto' : address,
+        'subject' : subject,
+        'content' : content
+    }
+
+    response = requests.post(url = url, data = data, headers = headers)
+    return response.json()['status']
 
 class SubscribeView(View):
     def post(self, request):
@@ -45,3 +61,36 @@ class UnsubscribeView(View):
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
         return JsonResponse({'message' : 'DELETE_SUCCESS'}, status = 200)
+
+class SendView(View):
+    def get(self, request, *args, **kwargs):
+        target = request.GET.get('target', None)
+
+        subject = 'Mail send TEST'
+        content = 'THIS IS JUST TEST'
+        if not(target):
+            address_list = Address.objects.all()
+
+            all_response = [{
+                'name' : address.name,
+                'email' : address.email,
+                'subject' : subject,
+                'content' : content,
+                'result' : send_mail(address.email, subject, content)
+            } for address in address_list]
+
+            return JsonResponse({'message' : all_response}, status = 200)
+
+        if target:
+            address = Address.objects.get(email = target)
+            if not(address):
+                return JsonResponse({'message' : 'WRONG_TARGET_NAME'}, status = 400)
+
+            response = {
+                'name' : address.name,
+                'subject' : subject,
+                'content' : content,
+                'result' : send_mail(address.email, subject, content)
+            }
+
+            return JsonResponse({'message' : response}, status = 200)
